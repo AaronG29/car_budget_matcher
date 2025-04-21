abc469149329cf072c7109d4c5a8cc0d5bd75d80
AaronG29 <aar010n@terpmail.umd.edu>
2025-04-20 22:36:15 -0400

    Added skeleton to car_data_test.py along with comments and docstrings

--- car_data_test.py
+++ car_data_test.py
@@ -1,3 +1,28 @@
 """
 This module will test the car dataset
 """
+
+from car_data import CarDatabase
+
+class TestCarDatabase():
+    """Test for CarDatabase class"""
+    
+    def test_data(self):
+        """Test that data works"""
+        # This is where the test will go
+        pass
+    
+    def test_budget(self):
+        """Test that cars can be filtered by price"""
+        # This is where the test will go
+        pass
+
+    def test_make(self):
+        """Test that cars can be filtered by make"""
+        # This is where the test will go
+        pass
+    
+    def test_year(self):
+        """Test that cars can be filtered by year"""
+        # This is where the test will go
+        pass
