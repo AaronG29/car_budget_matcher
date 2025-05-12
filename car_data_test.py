5548e4347c5ae7ae83e5d9ef69e2f9e29f65a9f0
AaronG29 <aar010n@terpmail.umd.edu>
2025-05-12 16:44:56 -0400

    Added test_make and test_year

--- car_data_test.py
+++ car_data_test.py
@@ -19,21 +19,30 @@ class TestCarDatabase():
         
 
     def test_data(self):
-        """Test that data works"""
-        # This is where the test will go
-        pass
+        """Test for making sure that the data will load right"""
+        assert len(self.db.data) == 4
+        assert self.db.data[0]["Name"] == "Mazda 5"
     
     def test_budget(self):
-        """Test that cars can be filtered by price"""
-        # This is where the test will go
-        pass
+        """Test for filtering the cars by their max price"""
+        results = self.db.filter_by_budget(15000)
+        car_names = [car["Name"] for car in results]
+        assert "Ford Focus" in car_names
+        assert "Mazda 5" in car_names
+        assert "Toyota Corolla" in car_names
+        assert "Tesla Model Y" not in car_names
 
     def test_make(self):
-        """Test that cars can be filtered by make"""
-        # This is where the test will go
-        pass
-    
+        """Filter by the car make test"""
+        results = self.db.filter_by_make("Toyota")
+        assert len(results) == 1
+        assert results[0]["Name"] == "Toyota Corolla"
+
     def test_year(self):
-        """Test that cars can be filtered by year"""
-        # This is where the test will go
-        pass
+        """Filter by the car min year test"""
+        results = self.db.filter_by_year(2018)
+        car_names = [car["Name"] for car in results]
+        assert "Tesla Model Y" in car_names
+        assert "Toyota Corolla" in car_names
+        assert "Mazda 5" not in car_names
+        assert "Ford Focus" not in car_names
