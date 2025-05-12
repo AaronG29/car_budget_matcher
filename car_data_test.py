ba964e39b3b67a8650b181bce10ca1a7dec06103
AaronG29 <aar010n@terpmail.umd.edu>
2025-05-12 15:20:50 -0400

    setup_method for the Setup for the test data before the test run

--- car_data_test.py
+++ car_data_test.py
@@ -6,7 +6,18 @@ from car_data import CarDatabase
 
 class TestCarDatabase():
     """Test for CarDatabase class"""
-    
+
+    def setup_method(self):
+        """Setup for the test data before the test run"""
+        self.db = CarDatabase()
+        self.db.data = [
+            {"Name": "Mazda 5", "Year": "2016", "Miles": "50000", "Price": "11000"},
+            {"Name": "Tesla Model Y", "Year": "2022", "Miles": "10000", "Price": "45000"},
+            {"Name": "Toyota Corolla", "Year": "2020", "Miles": "30000", "Price": "15000"},
+            {"Name": "Ford Focus", "Year": "2012", "Miles": "90000", "Price": "7000"}
+        ]
+        
+
     def test_data(self):
         """Test that data works"""
         # This is where the test will go
