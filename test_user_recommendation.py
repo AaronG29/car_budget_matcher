6489dd8404521fa82b3f5706ed4dc6bee0199b5e
AaronG29 <aar010n@terpmail.umd.edu>
2025-05-13 18:57:40 -0400

    Fixed ascending price function and tweaked some inconsistencies in the code

--- test_user_recommendation.py
+++ test_user_recommendation.py
@@ -5,7 +5,6 @@ This module will be used to test the user recommendations
 from car_data import CarDatabase
 
 
-# can make this more efficient
 
 class TestRecommendations():
     """Tests user preference recommendations"""
@@ -18,26 +17,25 @@ class TestRecommendations():
     
     def test_recommendation_budget(self):
         """Test making sure that cars will match the users budget range"""
-        # This is where the test will go
         result = self.db.filter_by_budget(16000, 24000)
-        assert all(10000 <= float(car['Price']) <= 20000 for car in result)
+        assert all(16000 <= float(car['Price']) <= 24000 for car in result)
     
     def test_recommendation_make(self):
         """Test making sure that cars will match the users make/makes"""
-        # This is where the test will go
         result = self.db.filter_by_make(['toyota', 'honda'])
         assert all(car['Name'].split()[0].lower() in ['toyota', 'honda'] for car in result)
 
     def test_recommendation_year(self):
         """Test making sure that cars will match the users preferred year/years"""
-        # This is where the test will go
         result = self.db.filter_by_year(2016, 2018)
-        assert all(2015 <= int(car['Year']) <= 2023 for car in result)
+        assert all(2016 <= int(car['Year']) <= 2018 for car in result)
 
-    def test_sort_by_recommendation(self):
-        """Test that cars are sorted, the sorting will be done by price (least/greatest -- cheapest car - most expensive car)"""
-        # This is where the test will go
-        cars = self.db.filter_by_budget(5500, 11000)
-        sorted_result = self.db.sort_results(cars, sort_key='Price', reverse=False)
-        prices = [float(car['Price']) for car in sorted_result]
-        assert prices == sorted(prices)
+
+def test_sort_by_recommendation(self):
+    """Test that cars are sorted, the sorting will be done by price (least/greatest -- cheapest car - most expensive car)"""
+    cars = self.db.filter_by_budget(5500, 11000)
+    assert cars, "No cars found in the budget"  
+
+    sorted_result = self.db.sort_results(cars, sort_key='Price', reverse=False)
+    prices = [float(car["Price"]) for car in sorted_result]
+    assert prices == sorted(prices), "The cars are not sorted by their ascending prices" 
